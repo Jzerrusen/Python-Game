f72bcf0c9914ff07085dbb5761fc97f346ae73dc
JZerrusen <jzerrusen1@lakelandcollege.edu>
2026-04-14 21:04:25 -0500

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+
+    def __init__(self):
+        self.screen_width = 800
+        self.screen_height = 400
+        self.bg_color = (230,200,250)
+
+        self.ship_speed = 1.5
+        
